4eaee9a77120d03ec258679920b8f707f5378c76
grason <grasonbrennan@gmail.com>
2013-09-24 11:12:08 -0400

    starting commit #2

---
+++ ChatGUI.cpp
@@ -0,0 +1,18 @@
+/*
+ * ChatGUI.cpp
+ *
+ *  Created on: Sep 24, 2013
+ *      Author: brennang
+ */
+
+#include "ChatGUI.h"
+
+ChatGUI::ChatGUI() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ChatGUI::~ChatGUI() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ ChatGUI.h
@@ -0,0 +1,17 @@
+/*
+ * ChatGUI.h
+ *
+ *  Created on: Sep 24, 2013
+ *      Author: brennang
+ */
+
+#ifndef CHATGUI_H_
+#define CHATGUI_H_
+
+class ChatGUI {
+public:
+	ChatGUI();
+	virtual ~ChatGUI();
+};
+
+#endif /* CHATGUI_H_ */
